a329af0ffc61f02b8a852b8f0472c413dac59b3b
Tsuchikawa Toshiki <toshiki@Tsuchikawa-no-MacBook-Air.local>
2016-08-25 15:58:06 +0900

    contactmail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,7 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
   end
-  
+
   def new
     if params[:back]
       @contact = Contact.new(contacts_params)
@@ -10,18 +10,19 @@ class ContactsController < ApplicationController
       @contact = Contact.new
     end
   end
-  
+
   def create
      @contact = Contact.new(contacts_params)
     if @contact.save
       # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
       redirect_to root_path,notice: "お問い合わせありがとうございました！"
+      NoticeMailer.sendmail_contact(@contact).deliver
     else
       # 入力フォームを再描画します。
       render action: 'new'
     end
   end
-  
+
   def confirm
     @contact = Contact.new(contacts_params)
     render :new if @contact.invalid?
--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -7,8 +7,13 @@ class NoticeMailer < ApplicationMailer
   #
   def sendmail_blog(blog)
     @greeting = "Hi"
-
     mail to: "ttccdd24@gmail.com",
              subject: '【Achieve】ブログが投稿されました'
+  end
+
+  def sendmail_contact(contact)
+    @greeting = "Hi"
+    mail to: "user.email",
+             subject: 'お問い合わせ完了いたしました！'
   end
 end
