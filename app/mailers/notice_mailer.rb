f86f697fa018afc81e78528a22b30ba5dd51292b
Tsuchikawa Toshiki <toshiki@Tsuchikawa-no-MacBook-Air.local>
2016-08-25 16:42:58 +0900

    contactmail

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -13,7 +13,7 @@ class NoticeMailer < ApplicationMailer
 
   def sendmail_contact(contact)
     @contact = contact
-    mail to: "@contact.email",
+    mail to: @contact.email,
              subject: '【Achieve】お問い合わせ完了いたしました！'
   end
 end
