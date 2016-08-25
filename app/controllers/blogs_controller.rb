f0bdc5c4267b86e600d24d3163a4b9ce78500e05
Tsuchikawa Toshiki <toshiki@Tsuchikawa-no-MacBook-Air.local>
2016-08-25 15:12:58 +0900

    fix

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
   end
-  
+
   def new
     if params[:back]
       @blog = Blog.new(blogs_params)
@@ -12,22 +12,23 @@ class BlogsController < ApplicationController
       @blog = Blog.new
     end
   end
-  
+
   def create
     @blog = Blog.new(blogs_params)
     @blog.user_id = current_user.id
     if @blog.save
       # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
       redirect_to blogs_path, notice: "ブログを作成しました！"
+      NoticeMailer.sendmail_blog(@blog).deliver
     else
       # 入力フォームを再描画します。
       render action: 'new'
     end
   end
-  
+
   def edit
   end
-  
+
   def update
   if  @blog.update(blogs_params)
     redirect_to blogs_path, notice: "ブログを編集しました！"
@@ -39,13 +40,13 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice: "ブログを削除しました！"
-  end 
-  
+  end
+
   def confirm
     @blog = Blog.new(blogs_params)
     render :new if @blog.invalid?
   end
-  
+
   private
   def blogs_params
     params.require(:blog).permit(:title, :content)
