b3a7ea46f2f6bf82abed01a445ae4d3e8f75cd34
Tsuchikawa Toshiki <toshiki@Tsuchikawa-no-MacBook-Air.local>
2016-08-30 18:36:27 +0900

    test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,18 +6,14 @@ class CommentsController < ApplicationController
     @blog = @comment.blog
 
 
-@notification = @comment.notifications.build(user_id: @blog.user.id )
+@notification = @comment.notifications.build(user_id: @blog.user.id)
     # クライアント要求に応じてフォーマットを変更
     respond_to do |format|
       if @comment.save
         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
         format.json { render :show, status: :created, location: @comment }
         format.js { render :index }
-        unless @comment.blog.user_id == current_user.id
-        Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'comment_created', {
-          message: 'あなたの作成したブログにコメントが付きました'
-        })
-        end
+
         Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'notification_created', {
           uncreate_count: Notification.where(user_id: @comment.blog.user.id).count
         })
