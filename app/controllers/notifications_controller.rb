766b6212adbeee7e0c26f404b346cc3c83522bf2
Tsuchikawa Toshiki <toshiki@Tsuchikawa-no-MacBook-Air.local>
2016-08-30 18:51:03 +0900

    test.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,6 +4,5 @@ class NotificationsController < ApplicationController
     def index
       @notifications = Notification.where(user_id: current_user.id).order(created_at: :desc)
       @notifications.update_all(read: true)
-
     end
   end
